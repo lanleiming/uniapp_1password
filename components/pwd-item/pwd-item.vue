6111218a07162cc3f1779b29b59f59a26e949403
lanleiming <lanleiming@360.cn>
2020-08-21 18:48:46 +0800

    init

---
+++ components/pwd-item/pwd-item.vue
@@ -0,0 +1,48 @@
+<!-- 密码item选项 -->
+<template>
+	<view>
+		<uni-list >
+		<!--    <uni-list-item  v-for="(item,index) in items" :showArrow="false" class="item" :title="'账号：'+item.name" >
+				  <template v-slot:header>
+				       asdfasdf
+				  </template>
+			</uni-list-item> -->
+			   <uni-list-item title="aa123123">
+			        <template v-slot:right>
+			          垂直排列的大图模式列表， header 内容为插槽自定义的内容
+			        </template>
+			    </uni-list-item>
+			
+		</uni-list>
+	
+
+	</view>
+</template>
+
+<script>
+	export default {
+		props: {
+			data: {
+				 type: [Object,Array],
+			},
+		},
+		data() {
+			return {
+				items:[]
+			};
+		},
+		mounted() {
+			if(this.data){
+				if(this.data instanceof Array){
+					this.items = this.data;
+				}else{
+					this.items = [this.data];
+				}
+			}
+		}
+	}
+</script>
+
+<style>
+
+</style>
---
+++ pages/index/index.vue
@@ -0,0 +1,59 @@
+<template>
+	<view class="container">
+		<uni-search-bar class="uni-searchbar" clearButton="auto" @input="searchTxt" cancelButton="none"  ></uni-search-bar>
+		<!-- 单行内容显示 -->
+		<!-- <uni-list>
+		    <uni-list-item  title="列表文字" ></uni-list-item>
+		    <uni-list-item :disabled="true" title="列表禁用状态" ></uni-list-item>
+		</uni-list> -->
+		<!-- 标题卡片模式 -->
+		<uni-card title="博客园" :is-shadow="true" >
+			<pwd-item :data="items"></pwd-item>
+		</uni-card>
+		<!-- 自定义底部按钮 -->
+	<!-- 	<uni-card title="Dcloud" note="true">
+		    默认内容
+		    <template v-slot:footer>
+		        <view class="footer-box">
+		            <view>喜欢</view>
+		            <view>评论</view>
+		            <view>分享</view>
+		        </view>
+		    </template>
+		</uni-card> -->
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				items:[{
+					name:'liuge',
+					pwd:'admin123@'
+				},
+				{
+					name:'liuge1',
+					pwd:'admin123@@'
+				}],
+				href: 'https://uniapp.dcloud.io/component/README?id=uniui'
+			}
+		},
+		methods: {
+			searchTxt(e){
+				console.log(e);
+			}
+		}
+	}
+</script>
+
+<style>
+	.container {
+		padding: 20rpx 10rpx;
+		font-size: 14px;
+		line-height: 24px;
+	}
+	.uni-searchbar{
+		padding:17.77rpx 33.33rpx !important;
+	}
+</style>
